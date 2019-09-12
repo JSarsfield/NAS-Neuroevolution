839df7dbbf240764ee296f7880316de955706cbb
Joe Sarsfield <joe.sarsfield@gmail.com>
2019-09-12 17:09:33 +0100

    master and worker nodes are now automatically updated and started

--- hpc_initialisation.py
+++ hpc_initialisation.py
@@ -17,26 +17,37 @@ def initialise_hpc(worker_list):
     s.connect(("8.8.8.8", 80))
     ip = s.getsockname()[0]
     s.close()
-    try:
-        ray.init(address=ip+":54504")
-    except:
-        os.system("gnome-terminal -e 'bash -c \"ray start --head --redis-port=54504; exec bash\"'")
-        time.sleep(2)
-        ray.init(address=ip + ":54504")
+    # Restart master node
+    os.system("ray stop")
+    os.system("ray start --head --redis-port=54504")
+    ray.init(address=ip + ":54504")
     if worker_list is not None:
+        host_address = str(ip)+":"+"54504"
+        # Build modules for sending to worker nodes
+        path = os.path.dirname(os.path.realpath(__file__))
+        os.system("cd "+path+" | python setup.py sdist")
         worker_ips = []
         with open(worker_list+".csv") as csvfile:
             rows = csv.reader(csvfile, delimiter=",")
             next(rows, None)
             for row in rows:
-                worker_ips.append(row[0])
-        for ip in worker_ips:
-            setup_worker(ip)
+                worker_ips.append({"ip": row[0], "user": row[1], "pw": row[2]})
+        path += "/dist/"
+        for file in os.listdir(path):
+            if "NAS" in file:
+                pkg_name = file
+                break
+        for w in worker_ips:
+            setup_worker(w["ip"], w["user"], w["pw"], path, pkg_name, host_address)
     ray.register_custom_serializer(CPPNGenome, use_pickle=True)
 
 
-def setup_worker(ip):
+def setup_worker(ip, user, pw, path_to_pkg, pkg_name, host_address):
     # ssh to worker
     # pull latest code
     # connect worker to redis server
-    pass
+    host = user+"@"+ip
+    os.system("sshpass -p \""+pw+"\" scp -o StrictHostKeyChecking=no "+path_to_pkg+pkg_name+" "+host+":/tmp")
+    os.system("sshpass -p \""+pw+"\" ssh -o StrictHostKeyChecking=no "+host+" -t \"bash -l -c -i 'pip install /tmp/NAS-0.1.tar.gz'\"")
+    os.system("sshpass -p \""+pw+"\" ssh -o StrictHostKeyChecking=no "+host+" -t \"bash -l -c -i 'ray stop'\"")
+    os.system("sshpass -p \""+pw+"\" ssh -o StrictHostKeyChecking=no "+host+" -t \"bash -l -c -i 'ray start --address="+host_address+" --load-code-from-local'\"")
