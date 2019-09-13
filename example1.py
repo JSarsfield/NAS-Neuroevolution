4415216413ac9391616149ef6dd3b2e85417ce1d
Joe Sarsfield <joe.sarsfield@gmail.com>
2019-09-13 14:05:27 +0100

    updated hpc cmds

--- example1.py
+++ example1.py
@@ -11,10 +11,10 @@ from environment import EnvironmentReinforcement
 if __name__ == "__main__":
     evolution = Evolution(n_net_inputs=4,
                           n_net_outputs=2,
-                          pop_size=256,
+                          pop_size=512,
                           environment=EnvironmentReinforcement,
                           gym_env_string="BipedalWalker-v2",  # CartPole-v0 BipedalWalker-v2
-                          worker_list=None,  # "hpc_worker_list",
+                          worker_list="hpc_worker_list",  # "hpc_worker_list"
                           processes=64)
     evolution.begin_evolution()
     print("end")
